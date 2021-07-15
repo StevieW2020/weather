e0070c8aca9ad76e80efd304e2d14d084e3f4ae7
StevieW2020 <86674950+StevieW2020@users.noreply.github.com>
2021-07-15 13:41:09 -0230

    figuring out overcast

--- app.js
+++ app.js
@@ -1,139 +1,138 @@
-let now = new Date();
-let h3 = document.querySelector("h3");
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-let month = months[now.getMonth()];
-let day = days[now.getDay()];
-let date = now.getDate();
-let hour = now.getHours();
-if (hour < 10) {
-  hour = `0${hour}`;
+function formateDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[date.getMonth()];
+  let year = date.getFullYear();
+  return `${day} , ${month}, ${hour}:${minute} / ${year}`; 
 }
-let minute = now.getMinutes();
-if (minute < 10) {
-  minute = `0${minute}`;
-}
-let year = now.getFullYear();
-h3.innerHTML = `${day} , ${month} ${date}, ${hour}:${minute} / ${year}`;
-
-function displayOvercast() {
+function displayOvercast(response) {
   let overcastElement = document.querySelector("#overcast");
 
-  let overcastHTML = `<div class="row">`;
   let days = ["Thur", "Fri", "Sat", "Sun"];
+
+  let overcastHTML = `<div class="row">`;
+  
   days.forEach(function (day) {
     overcastHTML =
       overcastHTML +
       `
-  
-    <div class="col">
-      <div class="weather-overcast-date">${day}</div>
-      <img
-        src="http://openweathermap.org/img/wn/50d@2x.png"
-        alt=""
-        width="50"
-      />
-      <div class="weather-overcast-temperature">
-        <span class="weather-overcast-temperature-max"> 11° </span>
-        /
-        <span class="weather-overcast-temperature-min"> 9° </span>
-      </div>
-    </div>
-    `;
-  });
+       <div class="col">
+        <div class="weather-overcast-date">${day}</div>
+        <img
+         src="http://openweathermap.org/img/wn/50d@2x.png"
+         alt=""
+         width="50"
+        />
+        <div class="weather-overcast-temperature">
+         <span class="weather-overcast-temperature-max"> 11° </span>
+         /
+         <span class="weather-overcast-temperature-min"> 9° </span>
+        </div>
+       </div>
+      `;
+    });
 
   overcastHTML = overcastHTML + `</div>`;
   overcastElement.innerHTML = overcastHTML;
 }
 
+function getOvercast(coordinates); {
+  let apiKey = "f45e557c26b84f8d37509b2f9860a5f0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayOvercast);
+}
+
 function showTemperature(response) {
-  let temperatureElement = document.querySelector(".temperature");
+  let temperatureElement = document.querySelector("#temperature");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let cityElement = document.querySelector("#city");
   let iconElement = document.querySelector("#icon");
+  let dateElement = document.querySelector("#date");
 
   celsiusTemperature = response.data.main.temp;
 
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   descriptionElement.innerHTML = response.data.weather[0].description;
-  cityElement.innerHTML = `${response.data.name}`;
+  cityElement.innerHTML = response.data.name;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formateDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getOvercast(response.data.coord);
 }
 
-function searching(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#change-city");
+function search(city) {
   let apiKey = "f45e557c26b84f8d37509b2f9860a5f0";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
-  let h2 = document.querySelector("h2");
-  h2.innerHTML = `${searchInput.value}`;
-  axios.get(apiUrl).then(showTemperature);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", searching);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+function changeFahrenheit(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature");
+  let fahrenheit = (celsiusTemperature * 9) / 5 + 32;
+  temperature.innerHTML = Math.round(fahrenheit);
+}
 
 function changeCelsius(event) {
   event.preventDefault();
-  let temperature = document.querySelector(".temperature");
+  let temperature = document.querySelector("#temperature");
   temperature.innerHTML = Math.round(celsiusTemperature);
 }
 
 let celsiusTemperature = null;
 
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", changeCelsius);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
-function changeFahrenheit(event) {
-  event.preventDefault();
-  let temperature = document.querySelector(".temperature");
-  let fahrenheit = (celsiusTemperature * 9) / 5 + 32;
-  temperature.innerHTML = Math.round(fahrenheit);
-}
 let farrenheitLink = document.querySelector("#fahrenheit-link");
 farrenheitLink.addEventListener("click", changeFahrenheit);
 
-function showPosition(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let apiKey = "f45e557c26b84f8d37509b2f9860a5f0";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
-}
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", changeCelsius);
 
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(showPosition);
-}
-let currentButton = document.querySelector("#current-button");
-currentButton.addEventListener("click", getCurrentLocation);
-
-displayOvercast();
+search("St John's");
